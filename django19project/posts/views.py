da18b8f50942e5653684138a186553d42e40092d
jakubzimnol <jakubdodek@poczta.onet.pl>
2018-08-22 06:21:51 -0700

    add pagination to our blog

--- django19project/posts/views.py
+++ django19project/posts/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 from .models import Post
 from .forms import PostForm
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def post_create(request):
     form = PostForm(request.POST or None)
@@ -30,15 +30,30 @@ def post_detail(request, id=None):
 
 
 def post_list(request):
-    queryset = Post.objects.all()
-    context = {"post_list":queryset}
+    queryset_list = Post.objects.all()
+    paginator = Paginator(queryset_list, 5) # Show 25 contacts per page
+    page_request_var = 'page'
+    page = request.GET.get(page_request_var)
+    try:
+        queryset = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer, deliver first page.
+        queryset = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range (e.g. 9999), deliver last page of results.
+        queryset = paginator.page(paginator.num_pages)
+    context = {"queryset": queryset,
+              "title": "List",
+              'page_request_var': page_request_var}
     return render(request, "post_list.html", context)
-    
+
+
 def post_delete(request, id=None):
     instance = get_object_or_404(Post, id=id)
     instance.delete()    
     messages.success(request, "deleted success")
     return redirect("posts:list")
+
 
 def post_update(request, id=None):
     instance = get_object_or_404(Post, id=id)
