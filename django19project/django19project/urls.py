eb6ea952c948fb4589a20ff90b6be0f524c556b7
jakubzimnol <jakubdodek@poczta.onet.pl>
2018-09-09 02:16:26 -0700

    test

--- django19project/django19project/urls.py
+++ django19project/django19project/urls.py
@@ -18,7 +18,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
-#from posts import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
